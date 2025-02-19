d832679022994cbb6707a298efabc651278bae8b
Sakshi Parikh <parikh.saks@northeastern.edu>
2025-02-19 17:09:28 -0500

    Added output and variable files

---
+++ variables.tf
@@ -0,0 +1,13 @@
+variable "aws_region" {}
+variable "aws_profile" {}
+variable "vpc_cidr" {}
+variable "vpc_name" {}
+variable "public_subnets" {
+  type = list(string)
+}
+variable "private_subnets" {
+  type = list(string)
+}
+variable "availability_zones" {
+  type = list(string)
+}
