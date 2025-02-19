8c0bd300acf2c147b359a5aa9f6cb28f4875202e
Sakshi Parikh <parikh.saks@northeastern.edu>
2025-02-19 16:43:10 -0500

    Added Terraform main file

---
+++ main.tf
@@ -0,0 +1,102 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "~> 3.0"
+    }
+  }
+}
+
+provider "aws" {
+  region  = var.aws_region
+  profile = var.aws_profile
+}
+
+resource "random_string" "unique_suffix" {
+  length  = 4
+  upper   = false
+  numeric = true
+  special = false
+}
+
+resource "aws_vpc" "main" {
+  cidr_block           = var.vpc_cidr
+  enable_dns_support   = true
+  enable_dns_hostnames = true
+
+  tags = {
+    Name = "${var.vpc_name}-${random_string.unique_suffix.result}"
+  }
+}
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-igw-${random_string.unique_suffix.result}"
+  }
+}
+
+resource "aws_subnet" "public" {
+  count = length(var.public_subnets)
+
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.public_subnets[count.index]
+  map_public_ip_on_launch = true
+  availability_zone       = var.availability_zones[count.index]
+
+  tags = {
+    Name = "${var.vpc_name}-public-${count.index + 1}-${random_string.unique_suffix.result}"
+  }
+}
+
+resource "aws_subnet" "private" {
+  count = length(var.private_subnets)
+
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = var.private_subnets[count.index]
+  map_public_ip_on_launch = false
+  availability_zone       = var.availability_zones[count.index]
+
+  tags = {
+    Name = "${var.vpc_name}-private-${count.index + 1}-${random_string.unique_suffix.result}"
+  }
+}
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-public-rt-${random_string.unique_suffix.result}"
+  }
+}
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.main.id
+
+  tags = {
+    Name = "${var.vpc_name}-private-rt-${random_string.unique_suffix.result}"
+  }
+}
+
+resource "aws_route" "default_route" {
+  route_table_id         = aws_route_table.public.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.gw.id
+}
+
+resource "aws_route_table_association" "public_assoc" {
+  count          = length(var.public_subnets)
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "private_assoc" {
+  count          = length(var.private_subnets)
+  subnet_id      = aws_subnet.private[count.index].id
+  route_table_id = aws_route_table.private.id
+}
