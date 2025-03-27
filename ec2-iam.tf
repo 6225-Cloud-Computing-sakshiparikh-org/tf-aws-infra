7908b12cec9bbcc9066324168d7a4ae38f4bcc4b
Sakshi Parikh <parikh.saks@northeastern.edu>
2025-03-27 05:55:15 -0400

    Updated Terraform for cloudwatch

--- ec2-iam.tf
+++ ec2-iam.tf
@@ -48,3 +48,40 @@ resource "aws_iam_instance_profile" "ec2_profile" {
   name = "ec2-instance-profile"
   role = aws_iam_role.ec2_role.name
 }
+
+# Enhanced CloudWatch permissions
+resource "aws_iam_policy" "cloudwatch_access_policy" {
+  name        = "cloudwatch-access-policy"
+  description = "Policy for EC2 to send metrics/logs to CloudWatch"
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents",
+          "logs:DescribeLogStreams",
+          "cloudwatch:PutMetricData",
+          "cloudwatch:GetMetricStatistics",
+          "cloudwatch:ListMetrics",
+          "ec2:DescribeTags"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "cloudwatch_attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = aws_iam_policy.cloudwatch_access_policy.arn
+}
+
+# Add SSM permissions for CloudWatch agent
+resource "aws_iam_role_policy_attachment" "ssm_attach" {
+  role       = aws_iam_role.ec2_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
+}
